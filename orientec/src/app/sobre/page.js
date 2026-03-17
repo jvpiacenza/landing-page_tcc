63e396aa935e044ba8bf23c298dcc775dd948ebf
João Victor Piacenza de Oliveira Andrade <joaovictorpiacenza@gmail.com>
2026-03-17 13:53:21 -0300

    ver0

---
+++ orientec/src/app/sobre/page.js
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-[#dff0f5]">
+
+      <div className="flex flex-col bg-[#3491d9] items-center justify-center w-100 h-100 rounded-3xl">
+        <h1>Sobre</h1>
+      </div>
+
+    </div>
+  );
+}
